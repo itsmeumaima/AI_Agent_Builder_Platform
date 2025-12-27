3a7c15a57c37bb902af9c6919a01b673d56ab15d
itsmeumaima <umaimarauf2005@gmail.com>
2025-12-27 15:38:26 +0500

    authentication done

--- ai-agent-builder-platform/app/layout.tsx
+++ ai-agent-builder-platform/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Outfit } from "next/font/google";
-
-
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import {ClerkProvider} from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: "AI Agent Builder Platform",
@@ -18,12 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={outfit.className}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={outfit.className}
+        >
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- ai-agent-builder-platform/app/page.tsx
+++ ai-agent-builder-platform/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>Welcome to AI Agent builder platform</h2>
       <Button>Click me</Button>
+      <UserButton />
     </div>
   );
 }
